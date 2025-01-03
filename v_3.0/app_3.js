a39c7d6be68b957bbfdd81279e26e087a54d4f5e
ilazarkov <ilazarkov@gmail.com>
2025-01-02 21:17:40 -0600

    added work experiance

--- v_3.0/app_3.js
+++ v_3.0/app_3.js
@@ -1,13 +1,15 @@
 window.onload = (data) => {
     // console.log({data});
-    let current_section = 'home';
+
+    // Navigation tabs
+    let current_section = 'work';
     const actionButtons = document.querySelectorAll('#actions > button');
 
     actionButtons.forEach(button => {
         button.addEventListener('click', (e) => {
             //hide
             if (current_section !== '' && current_section !== button.getAttribute('to')) {
-                
+
                 const prevButton = document.querySelector(`button[to=${current_section}]`)
                 const newClass = prevButton.getAttribute('class').replace('active', '').trim()
                 prevButton.setAttribute('class', newClass);
@@ -19,7 +21,7 @@ window.onload = (data) => {
                 }
                 button.setAttribute('class', button.getAttribute('class').concat(' active'))
             }
-            
+
             current_section = button.getAttribute('to');
             const section = document.querySelector(`#${current_section}`);
             //make it appear
@@ -28,6 +30,31 @@ window.onload = (data) => {
             const newClazz = clazz.replace("hidden", "appear").replace("hide", '').trim()
             section.setAttribute("class", newClazz)
         })
+    })
 
+    //Work navigation
+    let current_work = 'accenture'
+    const work_buttons = document.querySelectorAll('#work nav button')
+    work_buttons.forEach(button => {
+
+        button.addEventListener('click', (e) => {
+            const to = button.getAttribute('to')
+            if (current_work !== to) {
+                //hide current work tile
+                const current_work_tile = document.querySelector(`div#${current_work}`)
+                let hide_clazz = current_work_tile.getAttribute('class').replace('appear', 'hide hidden');
+                current_work_tile.setAttribute('class', hide_clazz);
+                //make it visible
+                const work_tile = document.querySelector(`div#${to}`);
+                let appear_clazz = work_tile.getAttribute('class');
+                appear_clazz = appear_clazz.replace(' hidden', ' appear').replace('hide', '');
+                work_tile.setAttribute('class', appear_clazz);
+                //make the sellection as current work
+                const prev_button = document.querySelector(`#work nav button[to=${current_work}]`);
+                prev_button.setAttribute('class', prev_button.getAttribute('class').replace('active', '').trim())
+                button.setAttribute('class', 'active');
+                current_work = to;
+            }
+        })
     })
 };
