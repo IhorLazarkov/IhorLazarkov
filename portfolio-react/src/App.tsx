131cf70e8045b1c91c33599ffee32ca6d36bfba5
ilazarkov <ilazarkov@gmail.com>
2025-08-26 22:40:30 -0500

    add backbone

--- portfolio-react/src/App.tsx
+++ portfolio-react/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Navigation from './components/Navigation/Navigation'
+import HomePage from './components/HomePage/HomePage'
+
+const router = createBrowserRouter([
+  {
+    element: <Navigation />,
+    children: [
+      { path: "/", element: <HomePage /> }
+    ]
+  }
+])
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ portfolio-react/src/components/Navigation/Navbar.tsx
@@ -0,0 +1,16 @@
+import "./NavBar.css"
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>What I Do?</li>
+                <li>Work</li>
+                <li>Projects</li>
+                <li>Education</li>
+                <li>Awards</li>
+                <li>Conact Me</li>
+            </ul>
+        </nav>
+    );
+}
