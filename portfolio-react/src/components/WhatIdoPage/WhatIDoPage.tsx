8890f7173be98cf2e470730907edde3cb0321bf2
ilazarkov <ilazarkov@gmail.com>
2025-08-27 11:48:17 -0500

    added content and functionality to what i do page
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>

--- portfolio-react/src/components/WhatIdoPage/WhatIDoPage.tsx
+++ portfolio-react/src/components/WhatIdoPage/WhatIDoPage.tsx
@@ -1,7 +1,39 @@
+import "./WhatIDo.css"
+
+import { useState, type ReactElement } from "react";
+
 function WhatIDoPage() {
-    return (<section>
-        <h1>What I do</h1>
-    </section>);
+
+    const articles: Record<string, ReactElement> = {
+        "Full Stack Developer": <div>Story about Full Stack goes here</div>,
+        "Software Engineer": <div>Story about SE goes here</div>,
+        "Software Engineer in Test": <div>Story about SE in Testgoes here</div>,
+        "Automation Test Engineer": <div>Story about ATE goes here</div>,
+        "DevOps": <div>Story about DevOps goes here</div>
+    }
+
+    const [activeArticle, setActiveArticle] = useState(Object.keys(articles)[0])
+
+    return (
+        <section className="article">
+            <div className="what-i-do-container">
+                <div className="left-side">
+                    <ul>
+                        {Object.keys(articles).map((title) => {
+                            const isActive = title === activeArticle
+                            return <li
+                                key={title}
+                                className={isActive ? 'active' : ''}
+                                onClick={() => setActiveArticle(title)}
+                            >{title}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className="right-side">
+                    {articles[activeArticle]}
+                </div>
+            </div>
+        </section>);
 }
 
 export default WhatIDoPage;
