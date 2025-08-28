e9d4131a08182e0489fd8c57eb35945b20feb66c
ilazarkov <ilazarkov@gmail.com>
2025-08-27 19:53:03 -0500

    added education content

--- portfolio-react/src/components/EducationPage/EducationPage.tsx
+++ portfolio-react/src/components/EducationPage/EducationPage.tsx
@@ -1,7 +1,45 @@
+import "./Education.css"
+
 function EducationPage() {
-    return ( <section className="article">
-        <h1>Education Page</h1>
-    </section> );
+    return (<section className="article">
+        <h1>Univercity</h1>
+        <div className="education-tile">
+            <img src="diploma-gold.svg" alt="diploma image" />
+            <span>
+                <h3>Odessa National University I.I. Mechnikov</h3>
+                <h4>Masters in Computer Systems and Networks</h4>
+                <span>2005 - 2006</span>
+            </span>
+        </div>
+        <div className="education-tile">
+            <img src="diploma.svg" alt="diploma image" />
+            <span>
+                <h3>Odessa National University I.I. Mechnikov</h3>
+                <h4>Bachelor in Computer Systems and Networks</h4>
+                <span>2001 - 2005</span>
+            </span>
+        </div>
+        <h1>Post Second Education</h1>
+        <div className="education-tile">
+            <img src="SE_Diploma.png" alt="diploma image" />
+            <span>
+                <h3>App academy</h3>
+                <h4>Software Engineer</h4>
+                <span>2024 - 2025</span>
+            </span>
+        </div>
+        <h1>Courses</h1>
+        <div className="education-tile">
+            <img
+                src="performance-cert.jpeg"
+                alt="ceritiface image"
+            />
+            <span>
+                <h4>Performance Testing Course</h4>
+                <span>Oct - Dec 2021</span>
+            </span>
+        </div>
+    </section>);
 }
 
 export default EducationPage;
