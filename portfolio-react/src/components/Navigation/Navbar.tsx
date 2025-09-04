a4581fef46b0639b8fd739f1be62ec30536108d6
ilazarkov <ilazarkov@gmail.com>
2025-09-03 20:57:09 -0500

    added nav bar when mobile view
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>

--- portfolio-react/src/components/Navigation/Navbar.tsx
+++ portfolio-react/src/components/Navigation/Navbar.tsx
@@ -2,8 +2,101 @@ import "./NavBar.css"
 import { Link } from "react-router-dom";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 export default function NavBar() {
+
+    const getMenu = () => window.innerWidth <= 500
+        ? <MobileMenu />
+        : <DesktopMenu />
+
+    const [menu, setMenu] = useState(getMenu())
+    useEffect(() => {
+        const handleResize = () => setMenu(getMenu())
+        window.addEventListener("resize", handleResize)
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
+
+    return (
+        <>
+            {menu}
+        </>
+    );
+}
+
+function MobileMenu() {
+    const [isOpen, setOpen] = useState(false)
+    return (<>
+        <div role="navigation" aria-label="main navigation">
+            <div style={{ position: "relative" }}>
+                <div
+                    style={{
+                        cursor: "pointer",
+                        zIndex: "2",
+                        position: "fixed",
+                        top: "0",
+                        opacity: 1,
+                        width: "100%",
+                        backgroundColor: "var(--bg-color)",
+                        boxSizing: "border-box",
+                        padding: "0.5em"
+                    }}
+                    onClick={() => setOpen(!isOpen)}>
+
+                    {isOpen ? (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="32px"
+                            viewBox="0 -960 960 960"
+                            width="32px"
+                            style={{ fill: "var(--border-color)" }}                    >
+                            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                        </svg>
+                    ) : (
+                        <svg
+                            style={{ fill: "var(--border-color)" }}
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="32px"
+                            viewBox="0 -960 960 960"
+                            width="32px"
+                        >
+                            <path d="M120-680v-80h720v80H120Zm0 480v-80h720v80H120Zm0-240v-80h720v80H120Z" />
+                        </svg>
+                    )}
+                </div>
+                {isOpen && <nav
+                    role="navigation"
+                    style={{
+                        position: "fixed",
+                        left: "0",
+                        top: "20px"
+                    }}>
+                    <ul style={{
+                        listStyle: "none",
+                        padding: "0",
+                        display: "flex",
+                        flexDirection: "column",
+                        "gap": "1.2em",
+                        paddingTop: "1em",
+                        paddingLeft: "1em",
+                        paddingRight: "1em",
+                    }}>
+                        <li><Link onClick={() => setOpen(false)} to="/IhorLazarkov/">Home</Link></li>
+                        <li><Link onClick={() => setOpen(false)} to="/IhorLazarkov/whatido">What I Do?</Link></li>
+                        <li><Link onClick={() => setOpen(false)} to="/IhorLazarkov/experience">Experience</Link></li>
+                        <li><Link onClick={() => setOpen(false)} to="/IhorLazarkov/projects">Projects</Link></li>
+                        <li><Link onClick={() => setOpen(false)} to="/IhorLazarkov/education">Education</Link></li>
+                        <li><Link onClick={() => setOpen(false)} to="/IhorLazarkov/recognition">Recognition</Link></li>
+                    </ul>
+                </nav>}
+            </div>
+        </div>
+    </>);
+}
+
+function DesktopMenu() {
     return (
         <nav>
             <ul>
