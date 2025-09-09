1a3fd4c5a53acedd511e82ad1c21a0960cb0425e
ilazarkov <ilazarkov@gmail.com>
2025-09-08 19:15:10 -0500

    added ai button and styling
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>

--- portfolio-react/src/components/HomePage/HomePage.tsx
+++ portfolio-react/src/components/HomePage/HomePage.tsx
@@ -49,16 +49,17 @@ export default function HomePage() {
                         <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" /></svg>
                 </div>
             </div>
+            <div className="morph-container">
+                <div id="morph"></div>
+                <span>AI</span>
+            </div>
             <div style={{
                 position: "absolute",
                 bottom: "0",
-                fontSize:"0.6em"
+                fontSize: "0.5em"
             }}>
-                © {new Date().getFullYear()} Ihor Lazarkov. All rights reserved.</div>
-            {/* <ul>
-               <li><FontAwesomeIcon icon={faMobile}/> (615) 506 - 9411 <FontAwesomeIcon icon={faCopy}/></li>
-               <li><FontAwesomeIcon icon={faMailBulk}/> ilazarkov@gmail.com <FontAwesomeIcon icon={faCopy}/></li> 
-            </ul> */}
+                © {new Date().getFullYear()} Ihor Lazarkov. All rights reserved.
+            </div>
         </div>
     );
 }
