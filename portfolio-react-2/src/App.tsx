0ca1d4a39d599fe7523d7009a61623341163fe8e
ilazarkov <ilazarkov@gmail.com>
2026-04-07 20:47:18 -0500

    improved wording in warning massage
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>

--- portfolio-react-2/src/App.tsx
+++ portfolio-react-2/src/App.tsx
@@ -86,7 +86,7 @@ function App() {
             <svg xmlns="http://www.w3.org/2000/svg"
               viewBox="0 -960 960 960"
             ><path d="M330-120 120-330v-300l210-210h300l210 210v300L630-120H330Zm36-190 114-114 114 114 56-56-114-114 114-114-56-56-114 114-114-114-56 56 114 114-114 114 56 56Zm-2 110h232l164-164v-232L596-760H364L200-596v232l164 164Zm116-280Z" /></svg>
-            <span>Server temporarily is unavailable due to certificate expiration.</span>
+            <span>Server temporarily is unavailable due to expired certificate.</span>
           </div>
           <ClientToAgent />
         </div>
@@ -190,7 +190,7 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 -960 960 960"
               ><path d="M330-120 120-330v-300l210-210h300l210 210v300L630-120H330Zm36-190 114-114 114 114 56-56-114-114 114-114-56-56-114 114-114-114-56 56 114 114-114 114 56 56Zm-2 110h232l164-164v-232L596-760H364L200-596v232l164 164Zm116-280Z" /></svg>
-              <span>Server temporarily is unavailable due to certificate expiration.</span>
+              <span>Server temporarily is unavailable due to expired certificate.</span>
             </div>
             <ProjectCard key={proj.url} project={proj} />
           </div>
