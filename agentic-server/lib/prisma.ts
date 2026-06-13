f49380c9ea231bcca55c9e4002aedafcd0dd4f8f
ilazarkov@gmail.com <dev@MacMini.local>
2026-06-13 13:30:24 -0500

    fixes
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>

--- agentic-server/lib/prisma.ts
+++ agentic-server/lib/prisma.ts
@@ -1,10 +1,13 @@
-import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = process.env.NODE_ENV === "test" 
-? `${process.env.DATABASE_URL_QA}`
-: `${process.env.DATABASE_URL_PROD}`;
+const environment = process.env.NODE_ENV || 'qa';
+const envFile = environment === 'production' ? '.env' : `.env.${environment}`;
+
+import dotenv from  "dotenv"
+dotenv.config({ path: envFile });
+
+const connectionString = process.env["DATABASE_URL"] as string;
 
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
---
+++ agentic-server/prisma/seed.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../lib/prisma";
+import { type queriesModel as TQueries } from "../generated/prisma/models";
+import fs from 'node:fs'
+import { stat } from 'node:fs/promises'
+
+const prefix = 'backup_qa_'
+
+fs.readdir('../scripts', { withFileTypes: true }, (err, files) => {
+    //find latest file
+    if (err) throw new Error("error reading directory", err);
+    const latestFile = files
+        .filter(f => f.name.startsWith(prefix) && f.isFile())
+        .sort(async (a, b) => {
+            const stat1 = await stat(a)
+            const stat2 = await stat(b)
+            return stat1.ctimeMs - stat2.ctimeMs
+        })[0]
+    if (!latestFile) throw new Error("file was not found")
+    console.log("Found latest file:", { file: latestFile.name })
+
+    //write data to database
+    fs.readFile(`../scripts/${latestFile.name}`, async (err, data) => {
+        if (err) throw new Error("error reading file", err)
+        const json : TQueries[] = JSON.parse(data.toString())
+        try {
+            const result = await prisma.queries.createMany({ data: json })
+            console.log("created records: ", result.count)
+        } catch (err) {
+            throw new Error("error creating records", err)
+        }
+    })
+    //end
+})
