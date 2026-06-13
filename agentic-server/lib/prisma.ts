f907282459ade645b15b5a99a73ee899d55e30d7
ilazarkov@gmail.com <dev@MacMini.local>
2026-06-13 11:49:05 -0500

    added test using prisma not sequilize because sequelize's current stable version does not support sequilize-cli and idk how to make migration and all other stuff
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>

--- agentic-server/lib/prisma.ts
+++ agentic-server/lib/prisma.ts
@@ -2,7 +2,9 @@ import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.NODE_ENV === "test" 
+? `${process.env.DATABASE_URL_QA}`
+: `${process.env.DATABASE_URL_PROD}`;
 
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
