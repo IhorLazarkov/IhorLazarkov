cb49017cbc6ad367f2be173900726e7eca177339
ilazarkov@gmail.com <dev@MacMini.local>
2026-06-13 12:13:00 -0500

    added sctipt to backup
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>

--- agentic-server/db/deploy-10-13-2025.dsl
+++ agentic-server/db/deploy-10-13-2025.dsl
@@ -4,9 +4,3 @@ create table queries (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     body varchar(100) NOT NULL, 
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP);
-
-INSERT INTO queries (body) VALUES('Is this AI?');
-
-INSERT INTO queries (body) VALUES('How does this agent work?');
-
-INSERT INTO queries (body) VALUES('asdf');
---
+++ agentic-server/scripts/mackBackupQA.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../lib/prisma";
+import fs from 'node:fs'
+import path from 'node:path'
+
+const targetPath = path.join(__dirname, `backup_qa_${Date.now()}.json`)
+console.log({targetPath})
+
+prisma.queries.findMany()
+    .then(data => {
+        fs.writeFile(
+            `./scripts/backup_qa_${Date.now()}.json`,
+            Buffer.from(JSON.stringify(data)),
+            () => console.log("backup records: ", data.length))
+    })
+    .catch(err => {
+        console.log(err)
+    });
