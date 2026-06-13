509ed62c19535ae9887f0e6f57ce266b776c8402
ilazarkov@gmail.com <dev@MacMini.local>
2026-06-13 09:58:13 -0500

    get first 5 records
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>

--- agentic-server/test/test.spec.ts
+++ agentic-server/test/test.spec.ts
@@ -4,7 +4,11 @@ import assert from 'node:assert';
 import { prisma } from "../lib/prisma.ts";
 
 test("Test query db", async () => {
-  const queries = await prisma.queries.findMany();
-  console.log({queries});
-  assert(queries.length > 0);
+  const queries = await prisma.queries.findMany({
+    take: 5,
+    orderBy: {
+      createdAt: "desc",
+    }
+  });
+  assert.strictEqual(5, queries.length);
 })
