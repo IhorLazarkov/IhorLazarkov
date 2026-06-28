1e93836b2207e820cad60f349a75142cedbcf495
ilazarkov@gmail.com <dev@MacMini.local>
2026-06-28 12:30:06 -0500

    added rollback script
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>

--- agentic-server/release_06262026/deploy.sh
+++ agentic-server/release_06262026/deploy.sh
@@ -1,10 +1,10 @@
 export NODE_ENV=production
-pwd
+
 # Read Queries from and store them in a file
-# npx tsx ./release_06262026/read_db.ts
+npx tsx ./release_06262026/read_db.ts
 
 # Run Prisma migration
-# npx prisma migrate reset
+npx prisma migrate reset
 
 # Query LLM with questions and store its responses in DB
 npx tsx ./release_06262026/write_db.ts
