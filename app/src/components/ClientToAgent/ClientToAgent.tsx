d5c99ee47eb3a362ae95baff0d19aca9855a6cb8
Ihor Lazarkov <86078653+IhorLazarkov@users.noreply.github.com>
2026-06-28 16:42:04 -0500

    Dev (#68)
    * added documents
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * added service layer and tests
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * improved configurability
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * adding missed prisma clients and modules
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * added queries QA db file
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * corrected HTTP codes
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * add rag repository
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added rag service
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added RagService components
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added cache table and its repository
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added queries service and cache repository and service
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * rename
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added lms, ollama, server controllers
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added lmx studio; bug fix
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * reorg
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * Testing (#65)
    
    * refactoring
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added tests for live server
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    ---------
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * updated readme file
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * finisished with states (#66)
    
    * finisished with states
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * updated architecture diagram
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    ---------
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * fixed tests by removing augmenting a DB state in before/after sections
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * fixed caching logic and fix inbound message validation
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * completed integration with front end. many fixes. added output of stats of a request.
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * corrected name of imported service from lmsRouter
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added coverage flag for testing
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added refactoring in main.mjs
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * added server.stop when error occurred
    
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    
    * fixed script to run prod agentic server
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * prepared release script and performed release successfully.
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * cleanup
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * added rollback script
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * added logging to prisma
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * improved layout of stats of agent response
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * add error handling on client side
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    * added error handling on client side
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    
    ---------
    
    Signed-off-by: ilazarkov@gmail.com <dev@MacMini.local>
    Signed-off-by: ilazarkov <ilazarkov@gmail.com>
    Co-authored-by: ilazarkov@gmail.com <dev@MacMini.local>

--- app/src/components/ClientToAgent/ClientToAgent.tsx
+++ app/src/components/ClientToAgent/ClientToAgent.tsx
@@ -12,8 +12,8 @@ import './ClientToAgent.css'
 
 function ClientToAgent() {
 
-  const BASE_URL = 'https://agentic.ihorlazarkov-swe.in'
-  // const BASE_URL = 'http://localhost:3008'
+  // const BASE_URL = 'https://agentic.ihorlazarkov-swe.in'
+  const BASE_URL = 'http://localhost:3008'
   const MODEL_NAME = "qwen/qwen3-vl-4b"
   const [lettersCount, setCount] = useState(0)
 
@@ -26,6 +26,7 @@ function ClientToAgent() {
   type TResponse = {
     response: React.ReactElement[],
     topPrompts: { body: string }[],
+    error: string,
     stats: {
       input_tokens: string,
       tokens_per_second: string,
@@ -39,6 +40,10 @@ function ClientToAgent() {
     tokens_per_second: '',
     total_output_tokens: '',
     time_to_first_token_seconds: ''
+  }
+
+  function round(num: string): number {
+    return Number.parseFloat(Number.parseFloat(num).toFixed(2))
   }
 
   async function sendPrompt(_: TResponse, formData: FormData): Promise<TResponse> {
@@ -72,7 +77,8 @@ function ClientToAgent() {
           ...parseGemmaResponseToHtml(data.message)
         ],
         topPrompts: [...data.queries],
-        stats: { ...data.stats }
+        stats: { ...data.stats },
+        error: { ...data.error }
       };
 
     } catch (error) {
@@ -80,15 +86,15 @@ function ClientToAgent() {
 
       if (error instanceof Error && error.name === 'AbortError') {
         return {
-          response: [<span>Request Aborted by User.</span>], topPrompts: [], stats: INIT_STATS
+          response: [], topPrompts: [], stats: INIT_STATS, error: "User Aborted."
         };
       } else if (error instanceof Error) {
         return {
-          response: [<span>{error.message}</span>], topPrompts: [], stats: INIT_STATS
+          response: [], topPrompts: [], stats: INIT_STATS, error: error.message
         };
       }
       return {
-        response: [<span>An unknown error occurred.</span>], topPrompts: [], stats: INIT_STATS
+        response: [<span>An unknown error occurred.</span>], topPrompts: [], stats: INIT_STATS, error: JSON.stringify(error)
       };
     }
   }
@@ -118,7 +124,8 @@ function ClientToAgent() {
   const [answers, askQuestion, isPending] = useActionState(sendPrompt, {
     response: [],
     topPrompts: [],
-    stats: INIT_STATS
+    stats: INIT_STATS,
+    error: ""
   } as TResponse)
   const button = !isPending
     ? <button type="submit">
@@ -165,19 +172,33 @@ function ClientToAgent() {
                 data-testid="loader" />
             </div>
             : <>
-              {answers.response.map((answer, i) => <span key={i}>{answer}</span>)}
-              {answers.stats && <div style={{
-                backgroundColor: "lightgreen",
+              {/* Error */}
+              {Object.keys(answers.error).length > 1 && <span style={{
                 borderRadius: "10px",
                 width: "fit-content",
-                padding: "0.3em",
-                display: "flex"
-              }}>
-                <span>input tokens: {answers.stats.input_tokens}</span>
-                <span>tokens per sec: {answers.stats.tokens_per_second}</span>
-                <span>total tokens: {answers.stats.total_output_tokens}</span>
-                <span>time to first token: {answers.stats.time_to_first_token_seconds}</span>
-              </div>
+                padding: "0.3em 0.5em",
+                color: "red",
+                backgroundColor: "#edcfce"
+              }}>{answers.error}</span>}
+
+              {/* Response */}
+              {answers.response.map((answer, i) => <span key={i}>{answer}</span>)}
+
+              {/* Stats */}
+              {Object.keys(answers.error).length ===0 &&
+                answers.stats && <div style={{
+                  backgroundColor: "#cfedce",
+                  borderRadius: "10px",
+                  width: "fit-content",
+                  padding: "0.3em 0.5em",
+                  display: "flex",
+                  flexDirection: "column"
+                }}>
+                  <span>input tokens: {round(answers.stats.input_tokens)}</span>
+                  <span>tokens per sec: {round(answers.stats.tokens_per_second)}</span>
+                  <span>total tokens: {round(answers.stats.total_output_tokens)}</span>
+                  <span>time to first token sec: {round(answers.stats.time_to_first_token_seconds)}</span>
+                </div>
               }
             </>
           }
